75cd2ccceb9b29e83b809b9e00b03c30bc94651d
Cristian Almiron <cristianalmiron1988@gmail.com>
2020-10-19 17:08:06 -0300

    agrego la funcion obtener datos

--- js/main_index.js
+++ js/main_index.js
@@ -23,6 +23,43 @@ function cargarPagina(){
            
             let baseURL = 'https://web-unicen.herokuapp.com/api/groups/';
             let groupID = ' ';
-            l
+ 
+            function obtenerdatos(){
+        
+                let container=document.querySelector("tbody");
+                fetch(baseURL + groupID + "/" + collectionID ,{
+                    method: "GET",
+                    mode: "cors"
+                 })
+                .then (function (r){
+                    if(!r.ok){
+                        console.log("error")
+                    }
+                    else{
+                         return r.json()  
+                    }
+                    })
+                    .then (function (json){
+                        container.innerHTML = "";
+                        for (let data of json.productosregaleria){
+                            console.log(data.thing);
+                            container.innerHTML+='<tr id="tr-'+data._id+'">' + '<td>'+data.thing.precio + '</td>' + '<td class="' + 'busqueda">'+data.thing.articulo + '</td>'+ '<td>'+data.thing.caracteristicas + '</td>'+ '<td>'+data.thing.cantidad + '</td>'+ '<td>'+data.thing.pago + '</td>'+ '<td>'+data.thing.envio + '</td>' + '<td><button class="borrar" id="'+data._id+'">Eliminar</button></td>' +'<td><button class="editar" id="modificar-'+data._id+'"  data-toggle="modal" data-target="#exampleModal" >Editar</button></td>'+'</tr>';
+                        }
+                        let btnsborrar = document.querySelectorAll(".borrar");
+                        for(let i = 0; i < btnsborrar.length; i++){
+                            btnsborrar[i].addEventListener("click", borrarprod);
+                        }
+                        let btnseditar = document.querySelectorAll(".editar");
+                        for(let i = 0; i < btnseditar.length; i++){
+                            btnseditar[i].addEventListener("click", editarModal);
+                        }
+                   
+                    })
+                    .catch(function(e){
+                       console.log(e);
+                    })
+            }
+        
+           obtenerdatos();
 }  
 document.addEventListener("DOMContentLoaded", cargarPagina);
