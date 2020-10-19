64c90cdd3073f54e684341964767d6c34e29c980
Sofia Walter <sofiawalter8@gmail.com>
2020-10-19 16:55:17 -0300

    modifique carpeta js

--- js/main_index.js
+++ js/main_index.js
@@ -8,6 +8,21 @@ function cargarPagina(){
             elem.classList.toggle("oculto");
             }
         });
-    
+   
+    let arreglo_productos={
+                "thing":{
+                        "precio":2941,
+                        "articulo": "Cafetera Goteo",
+                        "caracteristicas":"1.8 Litros de capacidad - 14 tazas",
+                        "cantidad":"1 (Una)",
+                        "pago":"Efectivo,débito,crédito",
+                        "envio":"Todo el país"
+                    },
+        
+            };
+           
+            let baseURL = 'https://web-unicen.herokuapp.com/api/groups/';
+            let groupID = ' ';
+            l
 }  
 document.addEventListener("DOMContentLoaded", cargarPagina);
